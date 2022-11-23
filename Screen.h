cd560cfd96394a36f40ed5504074c01f15e64648
InoueSota <k022g0045@g.neec.ac.jp>
2022-11-23 19:03:58 +0900

    a

--- Screen.h
+++ Screen.h
@@ -115,7 +115,7 @@ public:
 	/// <param name="srcH">画像上の描画したい範囲縦幅</param>
 	/// <param name="textureHandle">テクスチャのハンドル</param>
 	/// <param name="color">描画するスプライトの色</param>
-	void DrawRectAngle(Vec2 Position, float Width, float Height, float srcX, float srcY, float srcW, float srcH, float textureHandle, unsigned int color);
+	void DrawRectAngle2(Vec2 Position, float Width, float Height, float srcX, float srcY, float srcW, float srcH, float textureHandle, unsigned int color);
 	/// <summary>
 	/// アスペクト比がウィンドウと同じ場合に使用する
 	/// </summary>
